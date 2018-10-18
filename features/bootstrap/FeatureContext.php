21df97eac1e34143c1a70a2c0887adabacf159f0
Rafael Alfaro <rafaela@thehangar.cr>
2018-10-17 21:24:35 -0600

    Define undefined steps

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,22 +1,41 @@
 <?php
 
-use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 
-/**
- * Defines application features from the specific context.
- */
-class FeatureContext implements Context
+class FeatureContext implements SnippetAcceptingContext
 {
-    /**
-     * Initializes context.
-     *
-     * Every scenario gets its own context instance.
-     * You can also pass arbitrary arguments to the
-     * context constructor through behat.yml.
-     */
-    public function __construct()
-    {
-    }
+  /**
+   * @Given there is a :arg1, which costs £:arg2
+   */
+  public function thereIsAWhichCostsPs($arg1, $arg2)
+  {
+    throw new PendingException();
+  }
+
+  /**
+   * @When I add the :arg1 to the basket
+   */
+  public function iAddTheToTheBasket($arg1)
+  {
+    throw new PendingException();
+  }
+
+  /**
+   * @Then I should have :arg1 product(s) in the basket
+   */
+  public function iShouldHaveProductInTheBasket($arg1)
+  {
+    throw new PendingException();
+  }
+
+  /**
+   * @Then the overall basket price should be £:arg1
+   */
+  public function theOverallBasketPriceShouldBePs($arg1)
+  {
+    throw new PendingException();
+  }
 }
